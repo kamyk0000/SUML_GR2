d34dbbd3aedaa4d3eeda95254096e89815148571
kamyk0000 <164790602+kamyk0000@users.noreply.github.com>
2025-06-05 18:53:29 +0200

    Update drug_app.py

--- App/drug_app.py
+++ App/drug_app.py
@@ -12,34 +12,34 @@ def predict_drug(age, sex, blood_pressure, cholesterol, na_to_k_ratio):
     label = f"Predicted Drug: {predicted_drug}"
     return label
 
+
 inputs = [
-        gr.Slider(15, 74, step=1, label="Age"),
-        gr.Radio(["M", "F"], label="Sex"),
-        gr.Radio(["HIGH", "LOW", "NORMAL"], label="Blood Pressure"),
-        gr.Radio(["HIGH", "NORMAL"], label="Cholesterol"),
-        gr.Slider(6.2, 38.2, step=0.1, label="Na_to_K"),
-    ]
+    gr.Slider(15, 74, step=1, label="Age"),
+    gr.Radio(["M", "F"], label="Sex"),
+    gr.Radio(["HIGH", "LOW", "NORMAL"], label="Blood Pressure"),
+    gr.Radio(["HIGH", "NORMAL"], label="Cholesterol"),
+    gr.Slider(6.2, 38.2, step=0.1, label="Na_to_K"),
+]
 outputs = [gr.Label(num_top_classes=5)]
 
 examples = [
-        [30, "M", "HIGH", "NORMAL", 15.4],
-        [35, "F", "LOW", "NORMAL", 8],
-        [50, "M", "HIGH", "HIGH", 34],
-    ]
-
+    [30, "M", "HIGH", "NORMAL", 15.4],
+    [35, "F", "LOW", "NORMAL", 8],
+    [50, "M", "HIGH", "HIGH", 34],
+]
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
-article = "This app is a part of the Beginner's Guide to CI/CD for Machine Learning. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
-
+article = ("This app is a part of the Beginner's Guide to CI/CD for Machine Learning. It teaches how to automate "
+           "training, evaluation, and deployment of models to Hugging Face using GitHub Actions.")
 
 gr.Interface(
-        fn=predict_drug,
-        inputs=inputs,
-        outputs=outputs,
-        examples=examples,
-        title=title,
-        description=description,
-        article=article,
-        theme=gr.themes.Soft(),
+    fn=predict_drug,
+    inputs=inputs,
+    outputs=outputs,
+    examples=examples,
+    title=title,
+    description=description,
+    article=article,
+    theme=gr.themes.Soft(),
 ).launch()
